1ef824b02a05fb9810eaea5148dd9eb306f8ad15
Enoma Deborah <deborah.enomah@andela.co>
2014-12-16 13:59:19 +0100

    modified

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
 var githubapp = angular.module('github', []);
 
 githubapp.controller('GithubController', function($scope, GithubService){
- $scope.results = [];
- $scope.getData = function(){
-  GithubService.getURL( $scope.text ).success( function( data ){
-    console.log(data);
-    console.log(data.length);
-    if(data.items.length > 0){
+  $scope.results = [];
+  $scope.getData = function(){
+    GithubService.getURL( $scope.text ).success( function( data ){
+      console.log(data);
+      console.log(data.length);
+      if(data.items.length > 0){
 
-      angular.forEach( data.items, function( users, index ){
-        $scope.results.push( users );
-        console.log( $scope.results);
-      });
-    }
-    else {
-        $scope.error="true";
+        angular.forEach( data.items, function( users, index ){
+          $scope.results.push( users );
+          console.log( $scope.results);
+        });
+      }
+      else {
+          $scope.error="true";
       }  
-        
-     });
- }
+          
+    });
+   }
 });
 
- githubapp.factory('GithubService', function($http){
-   var url =  "https://api.github.com/search/users";
-   var Info = {};
+githubapp.factory('GithubService', function($http){
+  var url =  "https://api.github.com/search/users";
+  var Info = {};
 
-   Info.getURL = function( query ){
-      return $http.get( url, { params: { q: query } });
-   };
+  Info.getURL = function( query ){
+    return $http.get( url, { params: { q: query } });
+  };
 
-   return Info;
- });
+  return Info;
+});
 
 
 
